1d7a8baf2f5c6748956d3b4caacab1683c39fe68
dibran_ho <dibran_ho@outlook.com>
2020-12-10 10:17:43 +0100

    Simple rest API using express

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const path = require('path');
+const moment = require('moment');
+const members = require('./Members');
+
 
 const app = express();
 
-app.get('/', (req, res) => {
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}:${moment().format()}`);
+    next();
+}
 
-});
+// Init middleware
+app.use(logger);
+
+// Get all Members
+app.get('/api/members', (req, res) => res.json(members));
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5000;
 
